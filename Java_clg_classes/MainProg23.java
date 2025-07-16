08527f1da1f53fe4f8f819437ff5334f6c129723
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-16 14:47:38 +0530

    end of the session

--- Java_clg_classes/MainProg23.java
+++ Java_clg_classes/MainProg23.java
@@ -1,20 +1,46 @@
             // Prog23
+// interface prog23 {
+//     void start();
+//     void stop();
+// }
+
+// class Car implements prog23 {
+//     public void start() {
+//         System.out.println("Car starts.");
+//     }
+//     public void stop() {
+//         System.out.println("Car stops.");
+//     }
+// }
+// class MainProg23 {
+//     public static void main(String[] args) {
+//         prog23 p = new Car();
+//         p.start();
+//         p.stop();
+//     }
+// }
+
+
+
+
+
+            // Prog23
 interface prog23 {
     void start();
     void stop();
 }
 
-class Car implements prog23 {
+class Bike implements prog23 {
     public void start() {
-        System.out.println("Car starts.");
+        System.out.println("Bike starts.");
     }
     public void stop() {
-        System.out.println("Car stops.");
+        System.out.println("Bike stops.");
     }
 }
 class MainProg23 {
     public static void main(String[] args) {
-        prog23 p = new Car();
+        prog23 p = new Bike();
         p.start();
         p.stop();
     }
