f27ff05aaa4791f382d7f243c046bb3d48eed64e
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-16 14:23:26 +0530

    qwoepiruqwpeoir

--- Java_clg_classes/prog22.java
+++ Java_clg_classes/prog22.java
@@ -7,6 +7,8 @@ abstract class prog22 {
     public static void main(String[] args) {
         cargo_plane cp = new cargo_plane();
         passanger_plane pp = new passanger_plane();
+        prog22 p = new cargo_plane();
+        p.fly();
         cp.carry();
         pp.carry();
     }
