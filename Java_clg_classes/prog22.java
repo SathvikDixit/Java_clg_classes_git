ab3af818f5681795047738a028705d8aa20a668b
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-16 14:18:18 +0530

    qowiuerpoiqwe

---
+++ Java_clg_classes/prog22.java
@@ -0,0 +1,25 @@
+abstract class prog22 {
+    public void fly() {
+        System.out.println("flying");
+    }
+    abstract void carry();
+
+    public static void main(String[] args) {
+        cargo_plane cp = new cargo_plane();
+        passanger_plane pp = new passanger_plane();
+        cp.carry();
+        pp.carry();
+    }
+}
+
+class cargo_plane extends prog22 {
+    public void carry() {
+        System.out.println("Carrying in cargo");
+    }
+}
+
+class passanger_plane extends prog22 {
+    public void carry() {
+        System.out.println("Carrying in passenger");
+    }
+}
