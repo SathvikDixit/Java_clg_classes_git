3a5b5f717935ad2e990648aa3d6729dcdfd05df8
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-17 13:02:26 +0530

    pqowriuqewruoi

--- Java_clg_classes/Day-4/prog31.java
+++ Java_clg_classes/Day-4/prog31.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 
 public class prog31 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the string: ");
+        String name = sc.nextLine();
+        String rev = "";
+        for(int i = name.length()-1; i >= 0; i--) {
+            rev += name.charAt(i);
+        }
+        System.out.print(rev);
+    }
 }
