7fa5ed2fcc83bb7aac6f1e1c3ac4d092402e94dd
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-17 12:32:05 +0530

    qowiuerwpqoei

---
+++ Java_clg_classes/Day-4/prog29.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class prog29 {
+    public static void main(String[] args) {
+
+        String[] names = new String[5];
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the 5 name: ");
+        for(int i = 0; i < 5; i++ ){
+            names[i] = sc.nextLine();
+        }
+        for(String st: names) {
+            if (st.charAt(0) == 'a') {
+                System.out.println(st);
+            }
+        }
+
+
+    }
+    
+}
