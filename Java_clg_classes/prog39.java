2a01525e155db891e72413f07b6607e6ab39f6f8
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-18 12:57:12 +0530

    qowieupqoweri

---
+++ Java_clg_classes/prog39.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class prog39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<String> f1 = new HashSet<>();
+        System.out.print("Enter your any 3 hobbies: ");
+        for (int i = 0; i < 3; i++) {
+            String name = sc.nextLine();
+            f1.add(name);
+        }
+
+        HashSet<String> f2 = new HashSet<>();
+        System.out.print("Enter your any 3 hobbies: ");
+        for (int i = 0; i < 3; i++) {
+            String name = sc.nextLine();
+            f2.add(name);
+        }
+
+        if(f1.retainAll(f2)) {
+            System.out.println("You have simillar hobbies."+f1);
+        }
+    }
+}
