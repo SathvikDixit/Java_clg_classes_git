bb206681fb619dee47334614d760cf903065339a
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-18 11:41:04 +0530

    fdsfsahjhj

---
+++ Java_clg_classes/prog36.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class prog36 {
+    public static void main(String[] args) {
+        HashSet<Integer> s = new HashSet<>();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        s.add(4);
+        s.add(5);
+        System.out.println(s);
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        System.out.println(a);
+    }
+}
