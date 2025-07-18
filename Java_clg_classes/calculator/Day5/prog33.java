984bf116e0dae9886a03d28ea8b6b3836c67cd38
SathvikDixit <sathvikdixit21@gmail.com>
2025-07-18 09:35:24 +0530

    wqwerouierouip

---
+++ Java_clg_classes/calculator/Day5/prog33.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class prog33 {
+    public static void main(String[] args) {
+     ArrayList<String> s = new ArrayList<>();
+    s.add("Sathvik");
+    s.add("Dixit");
+    System.out.println(s);   
+    }
+}
